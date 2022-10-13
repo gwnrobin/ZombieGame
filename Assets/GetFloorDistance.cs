0f34fcdb42825f857085f425bee9fd6a7899e151
gwnrobin <robinschellingerhout@live.nl>
2022-10-13 22:57:15 +0200

    Added blood

--- Assets/GetFloorDistance.cs
+++ Assets/GetFloorDistance.cs
@@ -11,10 +11,16 @@ public class GetFloorDistance : MonoBehaviour
     void Start()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, Mathf.Infinity))
         {
             float distanceToGround = hit.distance;
             visual.SetFloat("DistanceToGround", distanceToGround);
         }
+    }
+
+    private void Update()
+    {
+        transform.parent.transform.rotation = Quaternion.Euler(0, 0, 0);
+        Debug.DrawRay(transform.position, -Vector3.up, Color.yellow);
     }
 }
